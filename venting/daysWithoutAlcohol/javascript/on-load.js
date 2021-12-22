77115d31ec313447b083bfed0f1d603b068b4e88
Garz4 <fancygarz4@gmail.com>
2021-12-22 12:50:53 -0600

    Lint

--- venting/daysWithoutAlcohol/javascript/on-load.js
+++ venting/daysWithoutAlcohol/javascript/on-load.js
@@ -1,7 +1,7 @@
 const futureDate = 'Wed, 01 Jan 3000 12:00:00 UTC';
 const months = {
-  0:'Jan', 1:'Feb', 2:'Mar', 3:'Apr', 4:'May', 5:'Jun', 6:'Jul',
-  7:'Aug', 8:'Sep', 9:'Oct', 10:'Nov', 11:'Dic'
+  0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul',
+  7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dic'
 };
 const username = document.getElementById('username');
 const usernameInput = document.getElementById('username-input');
